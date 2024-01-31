3d034fbb99cb783fdb4392402c8dcca64c39aa7f
Martin Stefanov <MartinStefanov16@users.noreply.github.com>
2024-01-31 15:45:31 +0200

    solved problems

--- src/DefiningClasses_Excercise/P06PokemonTrainer/Main.java
+++ src/DefiningClasses_Excercise/P06PokemonTrainer/Main.java
@@ -22,11 +22,59 @@ public class Main {
             String pokemonElement = tokens[2];
             int pokemonHealth = Integer.parseInt(tokens[3]);
 
+            // create trainer and pokemon
+            // add poke to trainer
+            // add trainer to Map
+
+            Trainer trainer = new Trainer(trainerName);
+            Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+
+
+            if (!trainers.containsKey(trainerName)) {
+                // no trainer
+                trainers.put(trainerName, trainer);
+                trainer.putPokemon(pokemon);
+            } else {
+                trainers.get(trainerName).putPokemon(pokemon);
+            }
 
 
 
             input = scanner.nextLine();
         }
+
+        input = scanner.nextLine();
+
+        while (!input.equals("End")){
+            String element = input;
+
+            // check if trainer has poke with the element
+
+
+            trainers.entrySet().stream()
+                    .forEach(trainer ->{
+
+                        if (trainer.getValue().hasPokeWithElement(element)){
+                            // when yes, +1 badge
+                            trainer.getValue().addBadge();
+                        } else {
+                            // when no, -10hp on all pokemons
+                            trainer.getValue().removeHealth();
+                        }
+                        trainer.getValue().removeDeadPokemons();
+
+                    });
+
+
+            // remove killed pokemons
+
+            input = scanner.nextLine();
+        }
+        trainers.values().stream()
+                .sorted((t1,t2) -> Integer.compare(t2.getBadges(), t1.getBadges()))
+                .forEach(System.out::println);
+
 
 
 
--- src/DefiningClasses_Excercise/P06PokemonTrainer/Trainer.java
+++ src/DefiningClasses_Excercise/P06PokemonTrainer/Trainer.java
@@ -2,6 +2,7 @@ package DefiningClasses_Excercise.P06PokemonTrainer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Trainer {
     private String name;
@@ -12,5 +13,39 @@ public class Trainer {
         this.name = name;
         this.badges = 0;
         this.pokemons = new ArrayList<>();
+    }
+
+    public int getBadges() {
+        return badges;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d %d", name, badges , pokemons.size());
+    }
+
+    public void putPokemon(Pokemon pokemon){
+        pokemons.add(pokemon);
+    }
+
+    public boolean hasPokeWithElement(String element){
+        // get list
+        // get element from each poke obj
+        return this.pokemons.stream()
+                .map(Pokemon::getElement)
+                .collect(Collectors.toList())
+                .contains(element);
+    }
+
+    public void removeHealth() {
+        this.pokemons.forEach(pokemon -> pokemon.setHealth(pokemon.getHealth() - 10));
+    }
+
+    public void removeDeadPokemons(){
+        this.pokemons.removeIf(pokemon -> pokemon.getHealth() <= 0);
+    }
+
+    public void addBadge(){
+        this.badges += 1;
     }
 }
